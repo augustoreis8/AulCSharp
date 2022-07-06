c1df75df54caa1904ced0816bbd5ce94e6c7f28a
augustoreis8 <augustocrisostomo8@gmail.com>
2022-07-06 13:57:15 -0300

    Primeiro projeto CSharp

---
+++ ContaCorrente.cs
@@ -0,0 +1,25 @@
+public class ContaCorrente
+{
+    public string Titular;
+    public int Agencia;
+    public int Numero;
+    public double Saldo;
+
+    public ContaCorrente(string Titular, int Agencia, int Numero, double Saldo)
+    {
+        this.Titular = Titular;
+        this.Agencia = Agencia;
+        this.Numero = Numero;
+        this.Saldo = Saldo;
+    }
+
+    public bool Sacar(double ValorSaque)
+    {
+        if (this.Saldo >= ValorSaque)
+        {
+            this.Saldo = this.Saldo - ValorSaque;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace meuPrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta_do_Augusto = new ContaCorrente("Augusto", 2846, 2468, 2000);
+            ContaCorrente conta_do_Crisóstomo = new ContaCorrente("Crisóstomp", 19375, 13579, 3000);
+            ContaCorrente conta_do_Reis = new ContaCorrente("Reis", 12569, 010203, 1000);
+
+            Console.WriteLine("A conta é do (a) " + conta_do_Augusto.Titular +
+            ", a agência é " + conta_do_Augusto.Agencia + " e o número é "
+            + conta_do_Augusto.Numero + "Saldo: " + conta_do_Augusto.Saldo);
+
+            Console.WriteLine("A conta é do (a) " + conta_do_Crisóstomo.Titular +
+            ", a agência é " + conta_do_Crisóstomo.Agencia + " e o número é "
+            + conta_do_Crisóstomo.Numero + "Saldo: " + conta_do_Crisóstomo.Saldo);
+
+            Console.WriteLine("A conta é do (a) " + conta_do_Reis.Titular +
+            ", a agência é " + conta_do_Reis.Agencia + " e o número é "
+            + conta_do_Reis.Numero + "Saldo: " + conta_do_Reis.Saldo);
+
+            bool Sacar_conta = conta_do_Augusto.Sacar(500);
+            bool Sacar_conta1 = conta_do_Crisóstomo.Sacar(500);
+            bool Sacar_conta2 = conta_do_Reis.Sacar(500);
+
+            Console.WriteLine("A conta é do (a) " + conta_do_Augusto.Titular +
+            ", a agência é " + conta_do_Augusto.Agencia + " e o número é "
+            + conta_do_Augusto.Numero + "Saldo: " + conta_do_Augusto.Saldo);
+
+            Console.WriteLine("A conta é do (a) " + conta_do_Crisóstomo.Titular +
+            ", a agência é " + conta_do_Crisóstomo.Agencia + " e o número é "
+            + conta_do_Crisóstomo.Numero + "Saldo: " + conta_do_Crisóstomo.Saldo);
+
+            Console.WriteLine("A conta é do (a) " + conta_do_Reis.Titular +
+            ", a agência é " + conta_do_Reis.Agencia + " e o número é "
+            + conta_do_Reis.Numero + "Saldo: " + conta_do_Reis.Saldo);
+
+        }
+    }
+}
+
+
+
